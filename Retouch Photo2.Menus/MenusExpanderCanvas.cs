23c9f2c0432e7c2df8ee9d23b429a880bd5868b6
ysdy44 <ysdy44@outlook.com>
2021-02-27 05:05:08 +0800

    fix MenuButtonsControl

--- Retouch Photo2.Menus/MenusExpanderCanvas.cs
+++ Retouch Photo2.Menus/MenusExpanderCanvas.cs
@@ -1,18 +1,19 @@
-﻿// Core:              ★
+﻿// Core:              ★★
 // Referenced:   ★
-// Difficult:         
-// Only:              ★★★
-// Complete:      
+// Difficult:         ★★
+// Only:              ★★★★
+// Complete:      ★
 using System.Collections.Generic;
-using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Retouch_Photo2.Menus
 {
     /// <summary>
-    /// Represents a stack panel control that containing some <see cref="MenuButton"/>。
+    /// Represents a canvas control, that containing some <see cref="Expander"/>。
     /// </summary>
-    public sealed partial class MenuButtonsControl : StackPanel
+    public class MenusExpanderCanvas : Canvas
     {
 
         /// <summary>
@@ -20,27 +21,61 @@ namespace Retouch_Photo2.Menus
         /// </summary>
         public IList<IMenu> Menus
         {
+            private get => this.menus;
             set
             {
                 foreach (IMenu menu in value)
                 {
-                    if (menu == null || menu.Button == null) continue;
+                    if (menu == null) continue;
 
-                    FrameworkElement element = menu.Button.Self;
-                    this.Children.Add(element);
+                    Expander expander = menu.Self;
+                    expander.OverlayCanvas = this;
+                    this.Children.Add(expander);
                 }
+
+                this.menus = value;
+            }
+        }
+        private IList<IMenu> menus = null;
+
+        internal bool IsOverlayDismiss
+        {
+            set
+            {
+                if (value)
+                    this.Background = new SolidColorBrush(Colors.Transparent);
+                else
+                    this.Background = null;
             }
         }
 
         //@Construct
         /// <summary>
-        /// Initializes a MenuButtonsControl. 
+        /// Initializes a MenusExpanderCanvas. 
         /// </summary>
-        public MenuButtonsControl()
+        public MenusExpanderCanvas()
         {
-            this.Orientation = Orientation.Horizontal;
-            this.HorizontalAlignment = HorizontalAlignment.Center;
-            this.VerticalAlignment = VerticalAlignment.Center;
+            this.Tapped += (s, e) =>
+            {
+                foreach (IMenu menu in this.Menus)
+                {
+                    if (menu == null) continue;
+
+                    menu.HideLayout();
+                }
+                this.IsOverlayDismiss = false;
+            };
+
+            this.SizeChanged += (s, e) =>
+            {
+                foreach (IMenu menu in this.Menus)
+                {
+                    if (menu == null) continue;
+
+                    menu.CropLayout();
+                }
+                this.IsOverlayDismiss = false;
+            };
         }
 
     }
