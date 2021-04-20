a010762dad5e45bb3fa030b983c037fa75f97edd
ysdy44 <ysdy44@outlook.com>
2021-04-20 18:18:30 +0800

    [Stage] SizePicker

--- Retouch Photo2.Elements/Pickers/SizePicker.xaml.cs
+++ Retouch Photo2.Elements/Pickers/SizePicker.xaml.cs
@@ -3,6 +3,7 @@
 // Difficult:         ★
 // Only:              ★
 // Complete:      ★★
+using Microsoft.Toolkit.Uwp.UI;
 using Windows.Graphics.Imaging;
 using Windows.UI.Xaml.Controls;
 
@@ -19,23 +20,43 @@ namespace Retouch_Photo2.Elements
         public string WidthText { get => this.WidthTextBlock.Text; set => this.WidthTextBlock.Text = value; }
         /// <summary> HeightTextBlock's Text. </summary>
         public string HeightText { get => this.HeightTextBlock.Text; set => this.HeightTextBlock.Text = value; }
+
+        /// <summary> Minimum. Defult 16. </summary>
+        public int Minimum { get; set; } = 16;
+        /// <summary> Maximum. Defult 16384. </summary>
+        public int Maximum { get; set; } = 16384;
+
         /// <summary> Size. </summary>
-        public BitmapSize Size
+        public BitmapSize Size => new BitmapSize
         {
-            get => new BitmapSize()
-            {
-                Width = (uint)this.WidthPicker.Value,
-                Height = (uint)this.HeighPicker.Value
-            };
+            Width = (uint)this.SizeWith,
+            Height = (uint)this.SizeHeight,
+        };
+        /// <summary> Width for size. </summary>
+        public double SizeWith
+        {
+            get => (uint)double.Parse(this.WidthTextBox.Text);
             set
             {
-                this.WidthPicker.Value = (int)value.Width;
-                this.HeighPicker.Value = (int)value.Height;
+                double width = value;
+                if (width < this.Minimum) width = this.Minimum;
+                else if (width > this.Maximum) width = this.Maximum;
+                this.WidthTextBox.Text = width.ToString();
+            }
+        }
+        /// <summary> Height for size. </summary>
+        public double SizeHeight
+        {
+            get => (uint)double.Parse(this.HeightTextBox.Text);
+            set
+            {
+                double height = value;
+                if (height < this.Minimum) height = this.Minimum;
+                else if (height > this.Maximum) height = this.Maximum;
+                this.HeightTextBox.Text = height.ToString();
             }
         }
 
-        int cacheWidth = 1024;
-        int cacheHeight = 1024;
 
         //@Construct
         /// <summary>
@@ -45,6 +66,39 @@ namespace Retouch_Photo2.Elements
         {
             this.InitializeComponent();
 
+            this.WidthTextBox.Text = 1024.ToString();
+            TextBoxExtensions.SetDefault(this.WidthTextBox, 1024.ToString());
+            this.WidthTextBox.LostFocus += (s, e) =>
+            {
+                if (this.WidthTextBox.Text is string value)
+                {
+                    if (string.IsNullOrEmpty(value) == false)
+                    {
+                        double width = double.Parse(value);
+                        if (width < this.Minimum) this.WidthTextBox.Text = this.Minimum.ToString();
+                        else if (width > this.Maximum) this.WidthTextBox.Text = this.Maximum.ToString();
+                    }
+                }
+            };
+
+            this.HeightTextBox.Text = 1024.ToString();
+            TextBoxExtensions.SetDefault(this.HeightTextBox, 1024.ToString());
+            this.HeightTextBlock.LostFocus += (s, e) =>
+            {
+                if (this.HeightTextBlock.Text is string value)
+                {
+                    if (string.IsNullOrEmpty(value) == false)
+                    {
+                        double height = double.Parse(value);
+                        if (height < this.Minimum) this.HeightTextBlock.Text = this.Minimum.ToString();
+                        else if (height > this.Maximum) this.HeightTextBlock.Text = this.Maximum.ToString();
+                    }
+                }
+            };
+
+            /*
+        int cacheWidth = 1024;
+        int cacheHeight = 1024;
             this.WidthPicker.Unit = "px";
             this.HeighPicker.Unit = "px";
             this.WidthPicker.Minimum = 16;
@@ -75,6 +129,7 @@ namespace Retouch_Photo2.Elements
                 this.WidthPicker.Value = this.cacheWidth = (int)width;
                 this.cacheHeight = (int)height;
             };
+             */
 
         }
     }
