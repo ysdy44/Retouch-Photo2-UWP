d3d69df1c56ddb3cca7b782140e23126bf4c6dfb
ysdy44 <ysdy44@outlook.com>
2021-05-16 22:50:23 +0800

    Release: Strings

---
+++ Retouch Photo2.Elements/XMLs/XML.StringFactory.cs
@@ -0,0 +1,43 @@
+﻿// Core:              ★★★
+// Referenced:   ★★★
+// Difficult:         
+// Only:              
+// Complete:      ★
+using System.Collections.Generic;
+
+namespace Retouch_Photo2.Elements
+{
+    /// <summary>
+    /// Provide constant and static methods for XElement.
+    /// </summary>
+    public static partial class XML
+    {
+
+        /// <summary>
+        /// Create a ITool from an string and XElement.
+        /// </summary>
+        /// <param name="name"> The source name. </param>
+        /// <param name="strings"> The source strings. </param>
+        /// <param name="language"> The destination language. </param>
+        /// <returns> The created string. </returns>
+        public static string CreateString(string name, IDictionary<string, string> strings, string language)
+        {
+            if (strings is null) return name;
+
+            if (strings.ContainsKey(language)) return strings[language];
+
+            foreach (var item in strings)
+            {
+                if (language.Contains(item.Key))
+                {
+                    return item.Value;
+                }
+            }
+
+            if (name is null) return string.Empty;
+
+            return name;
+        }
+
+    }
+}
---
+++ Retouch Photo2.Elements/XMLs/XML.Strings.cs
@@ -0,0 +1,48 @@
+﻿// Core:              ★★★
+// Referenced:   ★★★
+// Difficult:         
+// Only:              
+// Complete:      ★
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Retouch_Photo2.Elements
+{
+    /// <summary>
+    /// Provide constant and static methods for XElement.
+    /// </summary>
+    public static partial class XML
+    {
+
+        /// <summary>
+        /// Saves the entire strings to a XElement.
+        /// </summary>
+        /// <param name="elementName"> The element name. </param>
+        /// <param name="strings"> The destination strings. </param>
+        public static XElement SaveStrings(string elementName, IDictionary<string, string> strings)
+        {
+            return new XElement(elementName,
+            (
+                from item
+                in strings
+                select new XElement("String", new XAttribute("Key", item.Key), new XAttribute("Value", item.Value))
+            ));
+        }
+
+        /// <summary>
+        ///  Loads a strings from an XElement.
+        /// </summary>
+        /// <param name="element"> The source XElement. </param>
+        /// <returns> The loaded strings. </returns>
+        public static IDictionary<string, string> LoadStrings(XElement element)
+        {          
+            return element.Elements("String").ToDictionary
+            (
+                item => item.Attribute("Key").Value,
+                item => item.Attribute("Value").Value
+            );
+        }
+
+    }
+}
