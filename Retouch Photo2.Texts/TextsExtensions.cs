ccf73cabf3ae4500ce2645363a2bdac20e6d3cd3
ysdy44 <ysdy44@outlook.com>
2021-06-11 11:23:34 +0800

    TextsExtensions

--- Retouch Photo2.Texts/TextsExtensions.cs
+++ Retouch Photo2.Texts/TextsExtensions.cs
@@ -3,15 +3,50 @@
 // Difficult:         
 // Only:              
 // Complete:      ★
+using Microsoft.Graphics.Canvas.Text;
+using System;
+using System.Linq;
+using Windows.Globalization;
 using Windows.UI.Text;
 
 namespace Retouch_Photo2.Texts
 {
     /// <summary>
-    /// Extensions of <see cref = "FontWeight2" />.
+    /// Extensions of <see cref="Retouch_Photo2.Texts"/>.
     /// </summary>
-    public static class FontWeightExtensions
+    public static class TextsExtensions
     {
+
+        /// <summary> Gets default font sizes. </summary>
+        public static float DefaultFontSizes = 20f;
+
+        /// <summary> Gets all font sizes. </summary>
+        public static float[] FontSizes = new float[] { 5f, 6f, 7f, 8f, 9f, 10f, 11f, 12f, 13f, 14f, 15f, 16f, 18f, 20f, 24f, 30f, 36f, 48f, 64f, 72f, 96f, 144f, 288f };
+
+        /// <summary>
+        /// Match the closest font size.
+        /// </summary>
+        public static float MatchingFontSize(float fontSize)
+        {
+            if (TextsExtensions.FontSizes.Contains(fontSize)) return fontSize;
+
+            float minSize = 5f;
+            float minAbs = float.MaxValue;
+            foreach (float size in TextsExtensions.FontSizes)
+            {
+                float abs = Math.Abs(size - fontSize);
+                if (minAbs > abs)
+                {
+                    minSize = size;
+                    minAbs = abs;
+                }
+            }
+
+            return minSize;
+        }
+
+        /// <summary> Gets all font families. </summary>
+        public static IOrderedEnumerable<string> FontFamilies = CanvasTextFormat.GetSystemFontFamilies(ApplicationLanguages.Languages).OrderBy(k => k);
 
         /// <summary>
         /// Turn into font wieght.
